e85b9683ddecf132d1d3a4f167a72926c48bc416
Pedro Passos <passosm.pedroh@gmail.com>
2024-07-31 10:36:35 -0300

    receba

---
+++ Aula05/Exer.php
@@ -0,0 +1,12 @@
+<?php  
+    function soma($n1,$n2) {
+        $soma = $n1 + $n2;
+        return $soma;        
+    }
+
+    function areaTriangulo($baseT, $alturaT) {
+        $areaT = ($baseT * $alturaT) / 2;
+    }
+
+
+?>
